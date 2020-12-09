0f8a4814137ed4e8ba55b9677dd1d115f58bdb8f
Ivan Reyes <ivanreyesconde@gmail.com>
2020-12-09 15:56:16 -0600

    Agregados modulos de i18n

--- public/config.template.js
+++ public/config.template.js
@@ -12,3 +12,24 @@ const CONFIG = {
     }
   }
 }
+
+const messages =  {
+  es: {
+    message: {
+      title: "Tesel — Sistema de Pagos",
+      header: "Sistema de pagos",
+      info: "Información",
+      client: "Cliente",
+      name: "Nombre o empresa",
+      emailaddress: "Email",
+      email: "micorreo@ejemplo.com",
+      conceptdescription: "Concepto",
+      concept: "¿De qué es este pago?",
+      cardinfo: "Pago con tarjeta",
+      amount: "Monto",
+      pay: "Pagar",
+      errorcopy: "Ocurrió un error desconocido. Por favor contáctanos.",
+      successcopy: "¡Muchas gracias, recibimos tu pago!",
+    }
+  }
+}
